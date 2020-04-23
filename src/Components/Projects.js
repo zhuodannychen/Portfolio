566f6deaaff85fc5fc8b594428c84ed19b8361c8
zhuodannychen <zhuodannychen@gmail.com>
2020-04-22 23:49:59 -0500

    edit projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, CardTitle, CardText, CardActions, Button, Grid, Cell } from 'react-mdl'
+import ProjectContainer from './ProjectContainer'
 
 class Projects extends Component {
     render() {
@@ -7,6 +7,31 @@ class Projects extends Component {
             <div className="projects-container" id={this.props.id}>
                 <h1 className="projects-title">PROJECTS</h1>
                 <hr style={{width: "10%", border: "1.5px solid black"}}></hr>
+                <div>
+                    <ProjectContainer 
+                    project_name="Timeline" 
+                    project_date="March 2020" 
+                    project_img="https://dficlub.org/wp-content/uploads/2019/09/dcfdsdf.jpg"
+                    project_body="This is my first project, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel malesuada mi. Integer ac turpis tempus sapien tempor viverra sit amet et velit. Donec laoreet risus id magna molestie porttitor. Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras tincidunt arcu risus, eu mattis dui vehicula id. Etiam vitae nisi eget nisl tempus condimentum vitae eleifend leo. Nulla volutpat arcu id neque gravida, id viverra tellus lacinia. Sed at feugiat tellus. Etiam hendrerit pulvinar diam, vitae facilisis risus dapibus eu. Nullam sed rutrum nisi."
+
+                    github_link="https://github.com/zhuodannychen/Timeline/"
+                    demo_link="https://zhuodannychen.github.io/Timeline/"
+                    />
+
+                    <ProjectContainer 
+                    project_name="Second Project" 
+                    project_date="April 2020" 
+                    project_img="https://dficlub.org/wp-content/uploads/2019/09/dcfdsdf.jpg"
+                    project_body="This is my second project, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel malesuada mi. Integer ac turpis tempus sapien tempor viverra sit amet et velit. Donec laoreet risus id magna molestie porttitor. Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras tincidunt arcu risus, eu mattis dui vehicula id. Etiam vitae nisi eget nisl tempus condimentum vitae eleifend leo. Nulla volutpat arcu id neque gravida, id viverra tellus lacinia. Sed at feugiat tellus. Etiam hendrerit pulvinar diam, vitae facilisis risus dapibus eu. Nullam sed rutrum nisi."
+                    />
+
+                    <ProjectContainer 
+                    project_name="Third Project" 
+                    project_date="April 2020" 
+                    project_img="https://dficlub.org/wp-content/uploads/2019/09/dcfdsdf.jpg"
+                    project_body="This is my third project, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel malesuada mi. Integer ac turpis tempus sapien tempor viverra sit amet et velit. Donec laoreet risus id magna molestie porttitor. Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras tincidunt arcu risus, eu mattis dui vehicula id. Etiam vitae nisi eget nisl tempus condimentum vitae eleifend leo. Nulla volutpat arcu id neque gravida, id viverra tellus lacinia. Sed at feugiat tellus. Etiam hendrerit pulvinar diam, vitae facilisis risus dapibus eu. Nullam sed rutrum nisi."
+                    />
+                </div>
 
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 20" preserveAspectRatio="none">
                     <polygon points="100,0 100,20 50,20"
