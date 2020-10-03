8c0af549f87b7619993e92d70ac403c531fc1665
zhuodannychen <zhuodannychen@gmail.com>
2020-10-02 23:09:18 -0500

    init

---
+++ src/Components/Main.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from 'react'
+import Navbar from "./Navbar"
+import Homepage from "./Homepage"
+import About from "./About"
+import Projects from "./Projects"
+import Contact from "./Contact"
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="body">
+                <Navbar />
+                <Homepage id="home"/>
+                <About id="about"/>
+                <Projects id="projects"/>
+                <Contact id="contact"/>
+            </div>
+        )
+    }
+}
+
+export default Main
