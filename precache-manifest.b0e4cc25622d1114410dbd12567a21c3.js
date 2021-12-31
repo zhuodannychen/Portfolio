802378372df3ff83208f460c645397fa03dd73fd
Danny Chen <zhuodannychen@gmail.com>
2021-12-30 20:57:26 -0600

    Updates

---
+++ precache-manifest.b0e4cc25622d1114410dbd12567a21c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab284dcc1543723d9f23cb0a8114f93",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "bf80d371a04e0780d8de",
+    "url": "/Portfolio/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "2432d114fe8d0a857a41",
+    "url": "/Portfolio/static/css/main.e7206422.chunk.css"
+  },
+  {
+    "revision": "bf80d371a04e0780d8de",
+    "url": "/Portfolio/static/js/2.5c26f7b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.5c26f7b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2432d114fe8d0a857a41",
+    "url": "/Portfolio/static/js/main.3907a48f.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "6cab09377ddb886c5287b6fa9ca3dff3",
+    "url": "/Portfolio/static/media/Zhuo_Chen_s_Resume.6cab0937.pdf"
+  },
+  {
+    "revision": "4ef97c0f5ab806260d60e2408f59b009",
+    "url": "/Portfolio/static/media/danny1_4_16_2020.4ef97c0f.jpg"
+  },
+  {
+    "revision": "ec9bc58f6cef07a156fd6ca0d672bf4b",
+    "url": "/Portfolio/static/media/pwsh_invitational.ec9bc58f.png"
+  }
+]);
