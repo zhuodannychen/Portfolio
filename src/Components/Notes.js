41da2f4a997a3a59682bd1e9cdff88a7c803d6a7
Danny Chen <zhuodannychen@gmail.com>
2022-01-10 19:55:35 -0600

    add google analytics

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import InvestingTerminologyNotes from "../Assets/Notes/Investing_Terminology_Notes.pdf"
+// import InvestingTerminologyNotes from "../Assets/Notes/Investing_Terminology_Notes.pdf"
 
 
 class Notes extends Component {
@@ -7,22 +7,23 @@ class Notes extends Component {
     return (
       <div className="notes-container container-fluid" id={this.props.id}>
         <h1 className="projects-title">Notes</h1>
-        <hr style={{width: "52px", border: "1.5px solid black", marginBottom: "60px"}}/>
+        <hr style={{width: "52px", border: "1.5px solid black", marginBottom: "25px"}}/>
+        <h5 style={{margin: "0 5% 40px 5%"}}>Notes from good things I have read.</h5> 
         <div className="row">
             <div className="notes-col col-sm ">
                 <h3>Tech</h3>
                 <ul>
-                    <li><a href={InvestingTerminologyNotes} target="blank">Investing notes</a> ... <span>December 2020</span></li>
-                    <li><a href={InvestingTerminologyNotes} target="blank">WInvesting notes</a> ... <span>December 2020</span></li>
+                    {/* <li><a href={InvestingTerminologyNotes} target="blank">Investing notes</a> ... <span>December 2020</span></li>
+                    <li><a href={InvestingTerminologyNotes} target="blank">WInvesting notes</a> ... <span>December 2020</span></li> */}
                 </ul>
             </div>
             <div className="notes-col col-sm ">
-                <h3>Finance</h3>
+                {/* <h3>Finance</h3>
                 <ul>
                     <li><a href={InvestingTerminologyNotes} target="blank">Investing notes</a></li>
                     <li><a href={InvestingTerminologyNotes} target="blank">Investing notes</a></li>
                 </ul>
-                <h3>Books</h3>
+                <h3>Books</h3> */}
             </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import GA4React from "ga-4-react"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,15 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+try {
+    setTimeout(_ => {
+        const ga4react = new GA4React("G-C52WW6J2BG");
+        ga4react.initialize().catch(err => console.error(err));
+    }, 4000);
+} catch (err) {
+    console.error(err);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
