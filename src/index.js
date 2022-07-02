aaa8f050010f68384e1f05f537908d5d44de6813
Danny Chen <zhuodannychen@gmail.com>
2022-07-02 11:32:03 -0500

    testing ga4 bug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import GA4React from "ga-4-react"
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+ReactGA.send("pageview");
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,14 +15,15 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-try {
-    setTimeout(_ => {
-        const ga4react = new GA4React(process.env.REACT_APP_GOOGLE_ANALYTICS);
-        ga4react.initialize().catch(err => console.error(err));
-    }, 4000);
-} catch (err) {
-    console.error(err);
-}
+//try {
+    //setTimeout(_ => {
+        //const ga4react = new GA4React(process.env.REACT_APP_GOOGLE_ANALYTICS);
+        //ga4react.initialize().catch(err => console.error(err));
+    //}, 4000);
+    //console.log('GA working')
+//} catch (err) {
+    //console.error(err);
+//}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
