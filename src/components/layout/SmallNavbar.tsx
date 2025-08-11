502b4f12905de7d6cb5fe6e7e9043304d5d207c1
Danny Chen <zhuodannychen@gmail.com>
2025-08-10 23:07:38 -0700

    fix navbar issues

--- src/components/layout/SmallNavbar.tsx
+++ src/components/layout/SmallNavbar.tsx
@@ -10,18 +10,40 @@ const SmallNavbar: React.FC = () => {
 
   return (
     <nav className="nav navbar navbar-expand-lg">
-      <button style={{margin: "15px"}} className="custom-toggler navbar-toggler" type="button" onClick={handleNavCollapse}>
-        <span className="navbar-toggler-icon"></span>
+      <button className="custom-toggler navbar-toggler" type="button" onClick={handleNavCollapse}>
+        <span className="hamburger-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
       </button>
-      <div style={{padding: !isNavCollapsed ? "10px" : "0", background: "#111"}} className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse nav-items`}>
-        <li className='nav-itm'><Link activeClass="active" to="home" spy={true} smooth={true} offset={-72} duration={500}>HOME</Link></li>
-        <li className='nav-itm'><Link activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500}>ABOUT</Link></li>
-        <li className='nav-itm'><Link activeClass="active" to="experiences" spy={true} smooth={true} offset={-70} duration={500}>EXPERIENCES</Link></li>
-        <li className='nav-itm'><Link activeClass="active" to="notes" spy={true} smooth={true} offset={-70} duration={500}>WRITINGS</Link></li>
-        <li className='nav-itm'><Link activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500}>PROJECTS</Link></li>
-        <li className='nav-itm'><Link activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500}>CONTACT</Link></li>
-        <a className='nav-itm' href="/zhuo_chen_resume_09_01_2022.pdf" rel="noopener noreferrer" target="_blank">RESUME</a>
-        <a className='nav-itm' href="https://dannychen.substack.com/" rel="noopener noreferrer" target="_blank">BLOG</a>
+      <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse nav-items`}>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className='nav-itm' activeClass="active" to="home" spy={true} smooth={true} offset={-72} duration={500}>HOME</Link>
+          </li>
+          <li className="nav-item">
+            <Link className='nav-itm' activeClass="active" to="about" spy={true} smooth={true} offset={-70} duration={500}>ABOUT</Link>
+          </li>
+          <li className="nav-item">
+            <Link className='nav-itm' activeClass="active" to="experiences" spy={true} smooth={true} offset={-70} duration={500}>EXPERIENCES</Link>
+          </li>
+          <li className="nav-item">
+            <Link className='nav-itm' activeClass="active" to="notes" spy={true} smooth={true} offset={-70} duration={500}>WRITINGS</Link>
+          </li>
+          <li className="nav-item">
+            <Link className='nav-itm' activeClass="active" to="projects" spy={true} smooth={true} offset={-70} duration={500}>PROJECTS</Link>
+          </li>
+          <li className="nav-item">
+            <Link className='nav-itm' activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500}>CONTACT</Link>
+          </li>
+          <li className="nav-item">
+            <a className='nav-itm' href="/zhuo_chen_resume_09_01_2022.pdf" rel="noopener noreferrer" target="_blank">RESUME</a>
+          </li>
+          <li className="nav-item">
+            <a className='nav-itm' href="https://dannychen.substack.com/" rel="noopener noreferrer" target="_blank">BLOG</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
