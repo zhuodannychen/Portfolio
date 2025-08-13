46520859335c1a307c091c917bdf96e8c109ffa4
Danny Chen <zhuodannychen@gmail.com>
2025-08-12 22:09:16 -0700

    quality fixes

--- src/components/sections/Experiences.tsx
+++ src/components/sections/Experiences.tsx
@@ -6,14 +6,23 @@ interface Props {
 }
 
 const experienceList = experiences.map((experience, index) => 
-    <div key={index} className="experience-item">
-        <div className="experience-content">
-            <h4 className="experience-title">{experience.position}</h4>
-            <h5 className="experience-company">{experience.company}</h5>
-            <h6 className="experience-duration">{experience.duration}</h6>
-            <ul className="experience-description">
-                {experience.description.map((fact, idx) => <li key={idx}>{fact}</li>)}
-            </ul>
+    <div key={index} className="timeline-item">
+        <div className="timeline-content">
+            <div className="experience-card">
+                <h4 className="experience-title">{experience.position}</h4>
+                <h5 className="experience-company">{experience.company}</h5>
+                <h6 className="experience-duration">{experience.duration}</h6>
+                <ul className="experience-description">
+                    {experience.description.map((fact, idx) => <li key={idx}>{fact}</li>)}
+                </ul>
+                {experience.technologies.length > 0 && (
+                    <div className="experience-technologies">
+                        {experience.technologies.map((tech, idx) => (
+                            <span key={idx} className="tech-tag">{tech}</span>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     </div>
 )
@@ -24,7 +33,8 @@ const Experiences: React.FC<Props> = ({ id }) => {
             <h1 className="about-title">EXPERIENCES</h1>
             <hr style={{width: "90px", border: "1.5px solid black", marginBottom: "60px"}}/>
 
-            <div className="experiences-timeline">
+            <div className="timeline-container">
+                <div className="timeline-line"></div>
                 {experienceList}
             </div>
         </div>
