0ec8afe1dd04a5c9e6cfa29118982b652a6f2fa0
Danny Chen <zhuodannychen@gmail.com>
2025-09-13 10:56:36 -0700

    fix sitemap issue

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,12 @@
-export default function sitemap() {
+import { MetadataRoute } from 'next'
+
+export async function generateStaticParams() {
+  return [
+    { __metadata_id__: [] },
+  ];
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://dannychen.me',
