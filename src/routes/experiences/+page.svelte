5c6c3d85d8ee1a1b3758c6d31c85d1cbeed1b042
Danny Chen <zhuodannychen@gmail.com>
2025-11-03 21:29:05 -0800

    finished adding experience component and added work experience under experiences section

---
+++ src/routes/experiences/+page.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import Experience from './Experience.svelte';
+</script>
+
+<section class="layout-md py-12">
+  <h2 class="heading2">Work Experience</h2>
+
+  <Experience 
+    title='Software Engineer'
+    company='Meta'
+    url="https://meta.com"
+    dates="March 2024 - Current"
+    location="Menlo Park, CA"
+  >
+    <li>
+      Product Development
+    </li>
+  </Experience>
+
+  <Experience 
+    title='Software Engineer Intern'
+    company='Capital One'
+    url="https://capitalone.com"
+    dates="June 2023 - August 2023"
+    location="San Francisco, CA"
+  >
+    <li>
+      Implemented REST API endpoints for a system designed to uniquely identify web devices, prioritizing user privacy by eliminating reliance on device fingerprinting
+    </li>
+    <li>
+      Wrote automated tests; built Jenkins jobs to create AWS infra and deploy service in a coded manner
+    </li>
+  </Experience>
+
+  <Experience 
+    title='Teaching Assistant'
+    company='Texas A&M University'
+    url=""
+    dates="January 2023 - May 2023"
+    location="College Station, TX"
+  >
+    <li>
+      Supported 90+ students as a TA for a competitive programming class taught under Dr. John Keyser
+    </li>
+    <li>
+      Held weekly office hours, debugging sessions, and led discussions; received all positive feedback from student evaluations and quoted as the GOAT TA
+    </li>
+  </Experience>
+
+  <Experience 
+    title='Software Engineer Intern'
+    company='Capital One'
+    url="https://capitalone.com"
+    dates="June 2022 - August 2022"
+    location="Plano, TX"
+  >
+    <li>
+      Developed machine learning algorithms such as decision trees to identify hot leads out of 150K+ leads generated monthly
+    </li>
+    <li>
+      Performed feature extraction using information gain and tuned model with cross validation, with 80% accuracy
+    </li>
+    <li>
+      Architected serverless backend and created REST API endpoints
+    </li>
+  </Experience>
+</section>
---
+++ src/routes/experiences/Experience.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let title: string;
+  export let company: string;
+  export let url: string;
+  export let dates: string;
+  export let location: string;
+</script>
+
+<div class="my-12">
+  <h3 class="text-black text-lg leading-tight mb-1">
+    {title} at <a class="link" href={url}>{company}</a>
+  </h3>
+  <p class="font-light mb-2">
+    <span class="whitespace-nowrap">{dates}</span>
+    <span class="px-0.5 text-neutral-500">•</span>
+    <span class="whitespace-nowrap">{location}</span>
+  </p>
+    <ul
+      class="list-disc pl-7 marker:text-neutral-400 space-y-1"
+    >
+      <slot />
+    </ul>
+</div>
+
+
