32e969ffec513d61d708783fc055833a2f6e6838
Danny Chen <zhuodannychen@gmail.com>
2025-11-27 15:02:57 -0800

    attempt at setting up google analytics 4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,6 +21,23 @@
   });
 </script>
 
+<svelte:head>
+  <!-- Google tag (gtag.js) -->
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-C52WW6J2BG"
+  ></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-C52WW6J2BG");
+  </script>
+</svelte:head>
+
 <Seo
   title="Danny Chen"
   description="Home page of Danny Chen, a software engineer in the Bay Area"
